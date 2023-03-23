b64d303034d57a2e9cb952c4e8b64f10bc14ec07
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-23 19:38:00 -0300

    18. Habilite o botão Enviar após a validação do checkbox

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const loginBtn = document.getElementById('login-button');
+const submitButton = document.getElementById('submit-btn');
+const agreementBox = document.getElementById('agreement');
 
 const validateForm = () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,13 @@ const validateForm = () => {
 };
 
 loginBtn.addEventListener('click', validateForm);
+
+const enableButton = () => {
+  if (submitButton.getAttribute('disabled') === null) {
+    submitButton.setAttribute('disabled', '');
+  } else {
+    submitButton.removeAttribute('disabled');
+  }
+};
+
+agreementBox.addEventListener('change', enableButton);
